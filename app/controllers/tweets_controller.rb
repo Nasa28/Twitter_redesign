647f70088088a50216fa3e81a8608482178c9bd0
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-13 01:55:38 +0100

    generate tweets controller and set up routes

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,10 @@
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
   
+  get 'tweets/index'
+  get 'tweets/new'
+  get 'tweets/create'
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-  root to: "home#index"
+  root to: "tweets#index"
+  resources :tweets, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
