816016952df4bd7ec6c30289f606d126c4f97c78
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-13 15:11:35 +0100

    add associations between Tweet and User tables

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
   belongs_to :users
+  belongs_to :author, class_name: 'User'
+  validates :text, presence: true, length: { maximum: 240 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,uniqueness: true, format: {with:
                       VALID_EMAIL_REGEX, message: "Not a valid email" }
-  validates :username, presence: true
-  validates :full_name, presence: true
+  validates :username, presence: true, length: {minimum: 4, maximum: 20}
+  validates :full_name, presence: true, length: {maximum: 30}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tweets
+  has_many :tweets, class_name: 'Tweet', foreign_key: 'author_id'
 end
