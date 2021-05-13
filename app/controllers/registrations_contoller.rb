11febf9bcac049bf49480541bf5ed59ca87fc0a0
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-13 01:26:32 +0100

    add registrations controller

---
+++ app/controllers/registrations_contoller.rb
@@ -0,0 +1,11 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:username, :full_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:username, :full_name, :email, :photo, :cover_image, :password, :password_confirmation, :current_password)
+  end
+end
