e1f3bc467765edfe8fd38f2e0f031ca7d2b1adb5
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-13 13:51:09 +0100

    modify tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,uniqueness: true, format: {with:
+                      VALID_EMAIL_REGEX, message: "Not a valid email" }
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
