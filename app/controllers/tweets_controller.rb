4ca852e7919ea03cbb8b98a6eb04f9ea693df10d
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-13 16:27:00 +0100

    add navbar

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,26 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show new]
   def index
   end
 
   def new
+    @tweet = current_user.tweets.build(tweet_params)
   end
 
   def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      flash.now[:notice] = "Tweet created successfully"
+      redirect_to root_path
+    else
+      render :new
+
+    end
+  end
+
+  private
+
+  def tweet_params 
+    params.require(:tweet).permit(:text, :author_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tweets, class_name: 'Tweet', foreign_key: 'author_id'
+  has_many :tweets, class_name: 'Tweet', foreign_key: 'author_id', dependent: :destroy
 end
