0ba6e2ac9c585a7bdfe9dadeb54e7e87399ee823
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-14 09:02:09 +0100

    modify signup form

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+  belongs_to :users
+  validates :text, presence: true, length: {maximum: 200}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   
   
   resources :tweets
-  resources :tweets, only: [:create, :index]
   get 'new_tweet', to: 'tweets#new'
   post 'new_tweet', to: 'tweets#create'
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, controllers:  { registrations: 'users/registrations' }
   root to: "tweets#index"
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
