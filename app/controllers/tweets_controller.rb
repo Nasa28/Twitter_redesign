7289b87c6390e4e34a2e031603776868fa775be1
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-17 21:17:25 +0100

    test

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets or /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order('created_at DESC')
     @tweet = Tweet.new
   end
 
