a68d54051845f8280bb622c9870441fd68fccaf5
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-17 22:31:55 +0100

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: "tweets#index"
   
   resources :tweets
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
 
  
   devise_for :users, controllers:  { registrations: 'users/registrations' }
-  
+  resources :users, only: [:show]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
