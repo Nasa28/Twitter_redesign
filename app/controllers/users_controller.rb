3824686fc396c59fb416e6368ec6f1d8f544ef37
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-18 17:08:46 +0100

    add show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
     @tweet =Tweet.new
      @tweets = Tweet.all
-    @user = User.find(params[:id])
+  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   
   resources :tweets
   
-
- 
-  devise_for :users, controllers:  { registrations: 'users/registrations' }
-  resources :users, only: [:show]
- 
+  
+    devise_for :users, controllers:  { registrations: 'users/registrations' }
+  
+ resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
