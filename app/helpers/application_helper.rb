cfb2dacc2324ed60708c26ad4d22b3f103a0fb83
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-19 12:54:03 +0100

    add user_avatar helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def user_avatar (user, size =40)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "#{size}x#{size}!")
+    else
+      gravatar_image_url(user.email, size: size)
+    end, class: "form-control" 
+  end
 end
