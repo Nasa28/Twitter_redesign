3f120fb6abe7a4452bd828c9a21aca0a2e429c64
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-21 17:20:34 +0100

    move logic from view to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
       gravatar_image_url(user.email, size: size)
     end
   end
+
+  def profile_photo(user)
+     image_tag user_photo(user, 1000), class: "rounded-circle" if signed_in?
+  end
+
+  def background_image(user)
+    link_to image_tag cover_photo(user, 1000), class: "cover_image" if signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
     devise_for :users, controllers:  { registrations: 'users/registrations' }
   
  resources :users, only: [:show]
+
+ resources :users do
+  member do
+  get :following, :followers
+  end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
